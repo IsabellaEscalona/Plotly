fe0be7a99ac0393affdcd0504dd496d8f2e55ef9
Isabella Escalona <isabellaescalona0110@gmail.com>
2026-05-05 00:35:04 +0000

    Primer modelo de datos

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,90 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+import enum
+from sqlalchemy import String, Boolean, Enum
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+follower = db.Table(
+    'followers',
+    db.Column('users_followed', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('users_follower', db.Integer, db.ForeignKey('users.id'), primary_key=True)
+)
+
+
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(180), nullable=False, unique=True)
+    password = db.Column(db.String(180), nullable=False)
+
+    perfil = db.relationship('Profile', backref='user', uselist=False)
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            'username': self.username
             # do not serialize the password, its a security breach
         }
+
+
+class Enum_Artist(enum.Enum):
+    COMIC_ARTIST = 'comic artist'
+    WRITER = 'writer'
+    HYBRID = 'hybrid'
+    READER = 'reader'
+
+
+class Profile(db.Model):
+    __tablename__ = 'profiles'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    profile_picture = db.Column(db.String(250))
+    bio = db.Column(db.String(500))
+    artist_type = db.Column(db.Enum(Enum_Artist), default=Enum_Artist.READER)
+    instagram = db.Column(db.String(180))
+    twitter = db.Column(db.String(180))
+    facebook = db.Column(db.String(180))
+    otros = db.Column(db.String(180))
+
+class Enum_Category_Post(enum.Enum):
+    ONLY_TEXT = 'only text'
+    COMIC = 'comic'
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(200))
+    category = db.Column(db.Enum(Enum_Category_Post), nullable=False)
+    description = db.Column(db.String(1000))
+
+class Enum_Type_Media(enum.Enum):
+    GIF = 'gif'
+    IMAGE = 'image'
+    VIDEO = 'video'
+    TEXT = 'text'
+
+class Content_Post(db.Model):
+    __tablename__ = 'contents_posts'
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    type = db.Column(db.Enum(Enum_Type_Media))
+    url = db.Column(db.String(200))
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    content = db.Column(db.String(1000))
+
+class Like(db.Model):
+    __tablename__ = 'Likes'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    liked = db.Column(db.Boolean, default=False)
