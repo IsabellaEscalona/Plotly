518fa7956ddb3979e64655ddcab77ba677ccf0b7
caruros <ccangana1@gmail.com>
2026-05-11 18:50:36 -0400

    se agrega endpoint signup, componentes Signup y Login en jsx,el store.js actualizado con token, falta el endpoint del login y revisar el componente como tal y agregar el perfil, recordar actualizar el VITE_BACKEND_URL en .env con la URL que les de para el backend (#1)

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,10 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Profile
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,20 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    body=request.json
+    if not body.get("email") or not body.get("contraseña") or not body.get("usuario"):
+        return jsonify({"error": "Email, usuario y  contraseña son obligatorios!"}), 400
+    existing_user = User.query.filter_by(email=body["email"]).first()
+    if existing_user:
+        return jsonify({"error": "User already exists"}), 400
+    new_user = User(email=body["email"], password=body["contraseña"], username=body["usuario"])
+    db.session.add(new_user)
+    db.session.flush()
+    new_profile = Profile(user_id=new_user.id)
+    db.session.add(new_profile)
+    db.session.commit()
+    return jsonify({"message": "Usuario creado correctamente!"}), 200
