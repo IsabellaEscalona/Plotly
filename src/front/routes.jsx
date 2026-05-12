36d55046861c4665b33eec0c160250250cba20cd
Isabella Escalona <isabellaescalona0110@gmail.com>
2026-05-12 01:31:42 +0000

    Componente del Profile

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -11,6 +11,7 @@ import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
 import { Signup } from "./pages/Signup";
 import { Login } from "./pages/Login";
+import { Profile } from "./pages/Profile";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -29,6 +30,7 @@ export const router = createBrowserRouter(
         <Route path="/demo" element={<Demo />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/me" element={ <Profile /> }></Route>
       </Route>
     )
 );
