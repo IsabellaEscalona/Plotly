d446251adb99d659d5bb5cf63045742930d51e79
caruros <ccangana1@gmail.com>
2026-05-27 20:09:14 -0400

    se agrega tiempo de token, actualizado a 8 horas (#17)
    * se agrega endpoint signup, componentes Signup y Login en jsx,el store.js actualizado con token, falta el endpoint del login y revisar el componente como tal y agregar el perfil, recordar actualizar el VITE_BACKEND_URL en .env con la URL que les de para el backend
    
    * se agrega endpoint de login. hash para las contraseñas y confirmacion de contraseña para signup
    
    * se agrega redireccion a /me luego de logearse y link a signup
    
    * se almacena user al hacer login
    
    * se agrega endpoint para cambiar la contraseña, falta agregar el componente jsx de éste.
    
    * se agrega endpoint de reestablecer contraseña, funciona con postman, falta agregar el componente .jsx
    
    * se agrega tiempo de token, 8 horas

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import JWTManager
-
+from datetime import timedelta
 # from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
@@ -32,6 +32,7 @@ else:
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "super-secret-key")
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=8)
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
 
